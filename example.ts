07a233ae94e94c7ac2f4ab6ff1e8e98658d289a7
[elio] <[castels@hotmail.it]>
2022-09-28 15:48:04 +0100

    fine lezione

--- example.ts
+++ example.ts
@@ -1,34 +1,59 @@
-// andiamo ad imparare ora i 'Any', 'unknown' and 'never' types
-// mettiamo caso ho un aggetto con una proprietà di una stringa
-let country:any = {name:"Italy"};
-// se io tentassi di modificare italy in un altra avro un errore => Il tipo 'string' non è assegnabile al tipo '{ name: string; }'
-// country = "colombia"
+// in questa lezione andremo a vedere 3 diversi modi per descrivere oggetti con i type
+// adniamo a creare un oggetto
+// noteremo che da questo oggetti ritoneranno numero errori 
+// il primo che manca la proprieta name all'oggetto 
+// e il secondo che le proprieta hanno come type any 
+// per risolvere questi problemi andiamo ad assegnare i type alle proprieta e ad aggiungere la proprietà name
+//avendo aggiunto la proprietà name dobbiamo assegnare un type anche ad essa
 
-// se tentassi di aggiungere un proprietà avrei cmq un errore => La proprietà 'code' non esiste nel tipo '{ name: string; }'.
-// country.code = "IT"
+const country:{name: string, code:string, population: number} = {
+name: "italia",
+code: "IT",
+population: 58964
+}
+//anche in questa funzione noteremo che country ha 
+// value come any quindi anche qui possiamo aggiungere un aggetto do type 
+// riferito a name e popoulation
+function displayCountry(country:{name:string, population:number}){
+    console.log(country.name.toUpperCase())
+    console.log(country.population * 10)
 
-//per risolvere questo problema aggiungendo a coutry:any = ecc
-country.code = "it";//adesso lo fa fare
+    displayCountry(country)
+}
 
 //------------------------------------------------------------------------------//
+//noteremo che adesso abbiamo sviluppato due controlli di type ma che in realta sono molto 
+// simili in realta ci sarebbe il modo di creare un controllo dei type unico per entrambi
+//questo secondo modo si chiama Interfaces
 
-//Vediamo il unknown
-// se provassi a misurare la lunghezza della proprieta 
-// riceverei come errore=> L'oggetto è di tipo 'unknown'
-// possiamo correggere questo errore aggiungendo un if condizionale 
-// in cui confermiamo che value è una string
-// non mi dara piu errore
-function pippo (value:unknown){
-    if(typeof value === "string")
-    console.log(value.length)
-}
-pippo("a string")//=> 8 mi ritornerà la lunghezza
+// quello che possiamo fare e creare una interface con un nome e assegnargli il controllo dei type
+interface Country {name: string, code:string, population: number}
+// andiamo ad assegnare l'interface all'oggetto con : Country = ecc..
+const countrySecond : Country = {
+    name: "Spagna",
+    code: "es",
+    population: 9874
+    }
+// e possiamo anche sostituirlo direttamente nella funzione e le proprieta veddranno comunque prese dalla Interface
+    function displayCountrySecond(country: Country){
+        console.log(country.name.toUpperCase())
+        console.log(country.population * 10)
+    
+        displayCountry(country)
+    }
 
 //----------------------------------------------------------------------------//
-
-// Vediamo ora il never
-// mettiamo caso la function che mi ritorna un value
-function daiUnErrore(message: string):never{
-    throw new Error(`qualcosa è andato storto: ${message}`)
-}
-//per farsi che non ritorni mai il risultato dobbiamo aggiungere :never alla funzione
+//terzo metodo sono le allies molto semplice basta invece di scrivere interface 
+// si scrive type e il nome che vogliamo dare al nostro controllo dei type
+type Paese = {name: string, code:string, population: number}
+const countryThird : Paese = {
+    name: "Francia",
+    code: "fr",
+    population: 145896
+    }
+    function displayCountryThird(country: Paese){
+        console.log(country.name.toUpperCase())
+        console.log(country.population * 10)
+    
+        displayCountry(country)
+    }
