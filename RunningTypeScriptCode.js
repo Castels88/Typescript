1ea4c5151a0ce8e71f502e8dfdfa9b858f8567de
[elio] <[castels@hotmail.it]>
2022-09-28 10:06:58 +0100

    fine lezione

--- RunningTypeScriptCode.js
+++
@@ -1,4 +0,0 @@
-// questo sara il file convertito in Javascript
-function DescribeCurrency(countryName, currency) {
-    return "the currency of ".concat(countryName, " is the ").concat(currency.name, " (").concat(currency.code, ") ");
-}
