bf8d9f248e06baaba685e3d1618cd6ae4620e05b
[elio] <[castels@hotmail.it]>
2022-09-29 17:11:04 +0100

    fine lezione

--- example.ts
+++ example.ts
@@ -1,44 +1,51 @@
-// in questa lezione andremo a vedere cosa sono i generi e cosa fanno
-// mettiamo caso abbiamo una funzione con un parametro 
-function identity(value){
-    return value
+//in questa lezione vedremo come creare generic interface 
+// mettiamo caso io abbiao un inteface
+interface Currency<type> {
+    currency: type
 }
-// diamo alla funzione dei valori
-let esemp1 = identity("india")// => valore stringa 
-let esemp2 = identity(123456)// => valore numerico 
-let esemp3 = identity(true)// => valore boolean
+const obj1: Currency<string>={currency:"euro"}
+// perfetto in questo caso tutto andra per il meglio perche obj rispetta tutti i type di Currency
 
-// noteremo che nonostante abbiamo dato questi valori value risultera sempre come type "any" => function identity(value: any): any
-// questo accade perche non abbiamo dato un type alla nostra funzione
-// il metodo migliore per rendere la nostra funzione generica e questo si fa con le variali di type
-// si aggiunge mettendo dopo il nome della funzione questo <Type> ma in relata possiamo chiamare questa variabile come vogliamo
-// e dopo aver aggiunto la variabile possiamo specificarla dentro l'attributo function identity<Type>(value: Type)e dopo si mette pure dopo
-// le parantesi che dire qualsiasi type possa essere function identity<Type>(value: Type): Type{ecc...}
+// ma se crearssi un altro obj2 che al suo interno abbia un oggetto con altre prorprieta riferibili
+//a quella della precedente currency interface?
+const obj2: Currency<{name:string, code:string}>={currency:{name:"euro", code:"EU"}}//avro questo errore => Il tipo '{ name: string; code: string; }' non è assegnabile al tipo 'string'.ts(2322)
+//example.ts(4, 5): Il tipo previsto proviene dalla proprietà 'currency', dichiarata in questo punto nel tipo 'Currency'
 
-function identity2<Type>(value: Type): Type{
-    return value
+// quello che posiamo fare e rendere la nostra inteface generic aggiungendo dopo il nome Currency<type> e poi all'interno prorietà: type e poi
+// dobbiamo anche definirle il tipo di type nelle constanti che richiamano la interface const => obj1: Currency<string>={currency:"euro"}
+//per la obj2 attenzione abbiamo un oggetto quindi dobbiamo inserire tra i <> un oggetto {}
+
+//-------------------------------------//
+// un altra cosa che possiamo fare è dare alle allies le proprietà generiche in modo cosi da poterle utilizzare 
+// a seconda della constante 
+
+type CurrentString = Currency<string>;
+type CurrentOggetto = Currency<{name:string, code:string}>
+
+// facendo cosi potro direttamente assegnare le 2 type allies alle costanti
+const obj3: CurrentString = {currency:"dollaro"}
+const obj4: CurrentOggetto = {currency:{name:"dollaro",code:"$"}}
+
+
+//----------------------------------------------------------------------------//
+//vediamo come creare generic allies
+// la prima una normalissima allies con la sua costante che rispecchia tutti i type
+interface Currency2<type> {
+    currency: type
 }
-let esemp4 = identity2<string>("francia")// => adesso ritornerà il type string
-let esemp5 = identity2<number>(123)// => adesso ritornerà il type number
-let esemp6 = identity2<boolean>(false)// => adesso ritornerà il type boolean
-// esempio function identity2<"francia">(value: "francia"): "francia"
-// ma possiamo anche settare direttamente il tipo aggiungendo <string>,<number>,<boolean>
+const obj5: Currency2<string>={currency:"indian rupe"}
+// e una seconda costante che al suo interno ha un oggeto con nuove prorietà
+const obj6: Currency2<{name:string,code:string}>={currency:{name:"indian rupe", code:"INR"}} // riceveremo un errore perche una ha un type di stringa e l'altro invece ha un oggetto
 
-//--------------------------------------//
-
-//si puo rendere generica anche una arrow function 
-const identity3= <Type>(value: Type): Type => value 
-
-//-------------------------------------------------------------------------------------//
-
-// le type variabili posso anche essere piu di una nella funzione 
-// ad esempio a piu parametri della funzione voglio associare diversi tipi di variabili type
-// e per il ritorno del type un array ad esempio [value1: Type1, value2:Type2] e poi 
-// come rutorno di funzione un array dei 2 value
-function identity4<Type1,Type2>(value1:Type1,value2:Type2 ):[value1: Type1, value2:Type2]{
-    return [value1, value2]
+// come possimao risolvere questo problema?
+// come abbiamo fatto in precedenza andiamo ad aggiungere il type alla allies
+interface Currency3<type> {
+    currency: type
 }
-// quando richiamero la funzione avro i 2 type per i valori 
-let output = identity4(true, "Italia")// come risultato avro=> let output:[value1: boolean, value2:string]
-// e come ho fatto prima posso anche passargli gia dei type let output = identity4<boolen, string>(true, "Italia") => ma in questo modo 
-// se invece di italia mettessi un numero riceverei un errore
+// adesso creiamo 2 allies che abbiamo il generic type da associare alle constanti
+type CurrentString2 = Currency<string>;
+type CurrentOggetto2 = Currency<{name:string, code:string}>
+// e le andiamo ad associare direttamente alle constanti.
+const obj7: CurrentString2={currency:"indian rupe"}
+const obj8: CurrentOggetto2={currency:{name:"indian rupe", code:"INR"}} // riceveremo un errore perche una ha un type di stringa e l'altro invece ha un oggetto
+
