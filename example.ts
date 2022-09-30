fbfe998872fc8508029b0d33f4651c8c832b7890
[elio] <[castels@hotmail.it]>
2022-09-30 13:28:26 +0100

    fine lezione

--- example.ts
+++ example.ts
@@ -1,10 +1,19 @@
-// quando creaimo un array puo essere modificato di defaul
-// aggiungere,levare elementi cambiare il valore
-// vediamo come usare typescrit per modificare un array
-const country = ["italia","cina","francia"];
-// aggiungiamo un elemento con push 
-country.push("grecia");
-// ma esiste un metodo per evitare che il nostro array venga modificato ed e il READ-ONly 
-// prima cosa si specifica il typo di valori che ci sono nell'array  
-// davanti al nome costante country:Array<string> e poi si mette READ-ONLY davanti ad Array
-const country2:ReadonlyArray<string>=["italia","cina","francia"]
+// the tuple type viene usate specificamente negli array 
+// ad esempio quando abbiamo nell'array 2 type diversi come in questo caso
+const country:[string,number] = ["italia", 789654]
+// possiamo aggiungere direttamente i tuple type alla costante con :[string,numebr]
+// in questo caso se andassimo ad aggiungere un altro valore che sia stringa o numero sara 
+// accettato , e se andassimo a fare il console.logdi questo array
+// con la posizione dell'elemnto dell'array che vogliamo venga visulaizzato
+// ci spuntera 
+console.log(country[1])
+// possiamo anche associare il tuple a un allies 
+type CountruPopulation = [string, number]
+const country2:CountruPopulation = ["francia", 448648]
+
+//oppure possiamo creare un array type con la allies e all'interno
+//mettere piu array 
+const country3:Array<CountruPopulation>=[
+    ["italia", 789654],
+    ["francia", 448648]
+]
