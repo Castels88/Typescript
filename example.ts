5e7daa3c6a70301d381281c402eb0ea6f9e63122
[elio] <[castels@hotmail.it]>
2022-10-02 13:39:24 +0100

    fine lezione

--- example.ts
+++ example.ts
@@ -1,97 +1,82 @@
-// vediamo adesso come creare le classi 
-// prima di si scrive class il nome che vogliamo dare e poi {}
-// andiamo a compilare i campi della nostra classe con name: string
-class Country {
-name: string // altri modi per aggiungere il type puo essere name:""=> stringa / oppure name:string=""
-code:string
-constructor(name:string, code:string){
-this.name=name
-this.code=code
-}
-}
+// in questa lezione vedremo come estendere una classe da una vecchia gia esistente 
+// ad una totalmente nuova, questa nuova classe avra tutte le proprieta, valori e metodi che
+// esistevano nella vecchia classe.
+//creaimo una classe 
+class Country{
+    readonly name:string
+    readonly code: string
 
-// posso assegnare la classe a una costante in quento modo 
-const italia = new Country("italia","it")
-console.log(italia)// => riceveremo un oggetto vuoto
-//posso richimare cosi il nome della classe
-// italia.name = "italia"
-// se provassi a richiamare un altra proprietà avrei 
-// un errore perche al momento ho solo na proprietà
-// italia.code = "it"=> La proprietà 'code' non esiste nel tipo 'Country'.
-//ora che l'ho aggiunto va 
-// italia.code = "it"
+    languages: string[] = []
 
-// esiste un altro metodo delle classi per passar epamatri ed è il constructor()
-// dentro il constructop metod possiamo inserire i nostri parametri con i type
-// per richiamare i parametri del construtor si usa la keyword key e il nome del valore =  il nome del valore
-// noteremo che dopo faccio cio dovremmo compilare la new Country("italia","it")=> perche solo in questo modo 
-// possiamo compilare i valori del constructor
-
-//--------------------------------------------------------//
-// se mettimao caso io volessi rendere non modificabile i valori della mia classe 
-// mi basta aggiungere READ-ONLY alle proprieta della classe no costructor
-class Country2 {
-    readonly name: string // altri modi per aggiungere il type puo essere name:""=> stringa / oppure name:string=""
-    readonly code:string
-    constructor(name:string, code:string){
-    this.name=name
-    this.code=code
-    }
-    }
-
-    const francia = new Country2("francia","fr")
-
-//--------------------------------------------------------//
-// un altro metodo che possiamo richiamare all'niterno delle nostre classi sono i function metod
-// questa funzione language puo avere anche essa dei valori con dei type
-
-class Country3 {
-    readonly name: string 
-    readonly code:string
-    languages : string[] = []
-    constructor(name:string, code:string){
-    this.name=name
-    this.code=code
-    }
-    addLanguage(language:string){
-        this.languages.push(language)
-    }
-    }
-
-    const spagna = new Country3("spagna","es")
-    // possiamo aggiungere il valore direttamente richiamando la funzione
-    spagna.addLanguage("spagnolo")
-    spagna.addLanguage("catalano")
-    // possiamo anche dare multipli valori ma affinche venga letto nella nostra classe
-    // dobbiamo inserire language come valore della nostra class languages : string[] = [] => siccome sono di piu 
-    // li voglio mettere dentro un array 
-    // e poi di conseguenza dentro la funzione addLanguage devo dichiarare col this.languages e usare il push method  per
-    // aggiungere i valori all'array
-
-//---------------------------------------------------------------------------// 
-//un possiamo aggiungere quante function method vogliamo dentro la clase 
-// ad esempio andiamo a creare una funzione che richiama la proprietà language
-
-class Country4 {
-    name: string 
-    code:string
-    languages : string[] = []
-    constructor(name:string, code:string){
-    this.name=name
-    this.code=code
+    constructor(name:string,code:string){
+        this.name = name
+        this.code = code
     }
     addLanguage(language:string){
         this.languages.push(language)
     }
     describeLanguage():string{
-        return `the language in ${this.name} include ${this.languages.join(" , ")}`// vado a selezionare i linguaggi e sapendo che sono 
-        //degli array uso join per convertirli in stringhe 
+        return `the languages spoken in ${this.name} include ${this.languages.join(", ")}`
     }
-    }
+}
 
-    const usa = new Country4("usa","us")
-    // possiamo aggiungere il valore direttamente richiamando la funzione
-    usa.addLanguage("spagnolo")
-    usa.addLanguage("inglese")
-    const description =usa.describeLanguage()
-    console.log(description)
+// andiamo a creare una nuova classe alla quale estendere le vecchia 
+class CountryWithCurrency extends Country{
+//andiamo ad inserire nuove proprietà dentro
+// questa classe  per farlo dobbiamo creare il constructor
+// ed inserire i vecchi della vacchia classe senno dara errore + quello nuovo
+// siccome questo costructor ha delle ereditarietà dal vecchio
+// è impostp da Ts impostare la keyword super() prima di definire le
+// proprieta ed inserire le vecchie proprietà dentro il super(name,code) 
+// ultimo passo è definire la nuova proprietà fuori dal constructor in
+// modo da poterla definire
+currency:Currency
+constructor(name:string,code:string,currency:Currency){//assegnando Currency al constructor a linea 56 non avremo piu l'errore
+    super(name,code);
+    
+    this.currency = currency
+    }
+    //andiamo creare un metodo fucntion che descrivi la currency 
+    // la quale ritonerà una stringa di testo 
+    describeCurrency(): string {
+        return `the currency of ${this.name} is the ${this.currency.name} ${this.currency.symbol} ${this.currency.code}`
+    }
+    //andiamo a creare una descrizione che abbia tutti i metodi 
+    describe():string{
+        let descrizione = `the Country descprition: ${this.name}\n`;
+        descrizione += this.describeLanguage()+"\n";
+        descrizione += this.describeCurrency();
+        return descrizione
+    }
+}
+// andiamo a creare una costante che abbia un oggetto delle proprieta della currency 
+const spagnaCurrency= {
+    name:"euro",
+    code:"EU",
+    symbol:"$"
+}
+// noteremo che non è assegnato nessun tipo a queste proprieta andiamo ad associare una irterface
+interface Currency{
+    name:string,
+    code: string,
+    symbol:string
+    // adesso possiamo assegnare questa interface anche nel constructor della nostra nuova classe
+
+}
+// e lo andiamo ad inserire nella const Spagna che adesso richiede un a 3 proprietà
+// creaiamo una costante alla quale associeremo la nostra nuova classe
+const Spagna = new CountryWithCurrency("spagna","es",spagnaCurrency);// inserita avremo questo errore L'argomento di tipo '{ name: string; code: string; symbol: string; }' non è assegnabile al parametro di tipo 'string'
+console.log(Spagna)
+// vedremo nel console log che la cost spagna avra tutti parametri della precedente classe
+// possimao anche richiamare i metodi dentro la vecchia classe
+Spagna.addLanguage("spagnolo")
+Spagna.addLanguage("catalano")
+// oppure l'altro metodo 
+const SpagnaDescription = Spagna.describeLanguage();
+console.log(SpagnaDescription)
+
+const SpagnaDescriptionCurrency = Spagna.describeCurrency();
+console.log(SpagnaDescriptionCurrency)
+
+const spagnaFullDescription = Spagna.describe();
+console.log(spagnaFullDescription)
