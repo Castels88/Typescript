98710f7d2f0ead8768bf12efb192878950556306
[elio] <[castels@hotmail.it]>
2022-10-02 17:08:11 +0100

    fine lezione

--- example.js
+++ example.js
@@ -1,83 +1,39 @@
-// vediamo adesso come creare le classi 
-// prima di si scrive class il nome che vogliamo dare e poi {}
-// andiamo a compilare i campi della nostra classe con name: string
-var Country = /** @class */ (function () {
-    function Country(name, code) {
-        this.name = name;
-        this.code = code;
-    }
-    return Country;
-}());
-// posso assegnare la classe a una costante in quento modo 
-var italia = new Country("italia", "it");
-console.log(italia); // => riceveremo un oggetto vuoto
-//posso richimare cosi il nome della classe
-// italia.name = "italia"
-// se provassi a richiamare un altra proprietà avrei 
-// un errore perche al momento ho solo na proprietà
-// italia.code = "it"=> La proprietà 'code' non esiste nel tipo 'Country'.
-//ora che l'ho aggiunto va 
-// italia.code = "it"
-// esiste un altro metodo delle classi per passar epamatri ed è il constructor()
-// dentro il constructop metod possiamo inserire i nostri parametri con i type
-// per richiamare i parametri del construtor si usa la keyword key e il nome del valore =  il nome del valore
-// noteremo che dopo faccio cio dovremmo compilare la new Country("italia","it")=> perche solo in questo modo 
-// possiamo compilare i valori del constructor
-//--------------------------------------------------------//
-// se mettimao caso io volessi rendere non modificabile i valori della mia classe 
-// mi basta aggiungere READ-ONLY alle proprieta della classe no costructor
-var Country2 = /** @class */ (function () {
-    function Country2(name, code) {
-        this.name = name;
-        this.code = code;
-    }
-    return Country2;
-}());
-var francia = new Country2("francia", "fr");
-//--------------------------------------------------------//
-// un altro metodo che possiamo richiamare all'niterno delle nostre classi sono i function metod
-// questa funzione language puo avere anche essa dei valori con dei type
-var Country3 = /** @class */ (function () {
-    function Country3(name, code) {
-        this.languages = [];
-        this.name = name;
-        this.code = code;
-    }
-    Country3.prototype.addLanguage = function (language) {
-        this.languages.push(language);
-    };
-    return Country3;
-}());
-var spagna = new Country3("spagna", "es");
-// possiamo aggiungere il valore direttamente richiamando la funzione
-spagna.addLanguage("spagnolo");
-spagna.addLanguage("catalano");
-// possiamo anche dare multipli valori ma affinche venga letto nella nostra classe
-// dobbiamo inserire language come valore della nostra class languages : string[] = [] => siccome sono di piu 
-// li voglio mettere dentro un array 
-// e poi di conseguenza dentro la funzione addLanguage devo dichiarare col this.languages e usare il push method  per
-// aggiungere i valori all'array
-//---------------------------------------------------------------------------// 
-//un possiamo aggiungere quante function method vogliamo dentro la clase 
-// ad esempio andiamo a creare una funzione che richiama la proprietà language
-var Country4 = /** @class */ (function () {
-    function Country4(name, code) {
-        this.languages = [];
-        this.name = name;
-        this.code = code;
-    }
-    Country4.prototype.addLanguage = function (language) {
-        this.languages.push(language);
-    };
-    Country4.prototype.describeLanguage = function () {
-        return "the language in ".concat(this.name, " include ").concat(this.languages.join(" , ")); // vado a selezionare i linguaggi e sapendo che sono 
-        //degli array uso join per convertirli in stringhe 
-    };
-    return Country4;
-}());
-var usa = new Country4("usa", "us");
-// possiamo aggiungere il valore direttamente richiamando la funzione
-usa.addLanguage("spagnolo");
-usa.addLanguage("inglese");
-var description = usa.describeLanguage();
-console.log(description);
+//Enums permette di definire un set di tag value
+// 1) mettiamo caso in questo array con oggetti abbiamo 
+// 2) degli oggetti che hanno lo stesso continente ma tutti hanno la proprieta continente
+// 3) possimao usare gli enum per rappresentare tutti questi oggetti
+//4) gli enum si creano cosi 
+//5) all 'interno del enum mettiamo tutte le categorie che vogliamo che 
+// rappresentino gli oggetti
+//6) e andiamo ad assegnarli alle proprieta continente dell'array
+var Continent;
+(function (Continent) {
+    Continent[Continent["Europa"] = 0] = "Europa";
+    Continent[Continent["Africa"] = 1] = "Africa";
+    Continent[Continent["Oceania"] = 2] = "Oceania";
+    Continent[Continent["SudAmerica"] = 3] = "SudAmerica";
+    Continent[Continent["Asia"] = 4] = "Asia";
+})(Continent || (Continent = {}));
+var Country = [
+    {
+        name: "italia",
+        continent: Continent.Europa
+    },
+    {
+        name: "nigeria",
+        continent: Continent.Africa
+    },
+    {
+        name: "australia",
+        continent: Continent.Oceania
+    },
+    {
+        name: "colombia",
+        continent: Continent.SudAmerica
+    },
+    {
+        name: "spagna",
+        continent: Continent.Europa
+    },
+];
+console.log(Country);
--- example.ts
+++ example.ts
@@ -1,38 +1,54 @@
-// in questa lezione vedremo come creare classi generiche
-// al momento dentro languages puo essere inserito solo un array di stringhe 
-// e se volessimo inserire all'interno non solo stringhe?
-// dobbiamo rendere la nostra classe generica
-// vediamo come =>
-// 1) prima dobbiamo aggiungere un<Type> alla nostra classe che chiameremo languageType
-// 2) aggiungiamo questo type al metodo addLanguages
-// 3) a riga 13 andiamo a modificare languages in modo che accetti il type
-// in questo modo il nostro array prendere qualciasi type non solo stringhe
-// 4) ma se controlliamo il type Ts ci dara type language: unknown
-// 5) per ovviare a cio a riga 27 diamo a country in type string 
-class Country<LanguageType>{
-    public readonly name:string
-    protected readonly code: string 
-
-    languages: LanguageType[] = [] 
-
-    constructor(name:string,code:string){
-        this.name = name
-        this.code = code
-    }
-    addLanguage(language:LanguageType){ 
-        this.languages.push(language)
-    }
-    
+//Enums permette di definire un set di tag value
+// 1) mettiamo caso in questo array con oggetti abbiamo 
+// 2) degli oggetti che hanno lo stesso continente ma tutti hanno la proprieta continente
+// 3) possimao usare gli enum per rappresentare tutti questi oggetti
+//4) gli enum si creano cosi 
+//5) all 'interno del enum mettiamo tutte le categorie che vogliamo che 
+// rappresentino gli oggetti
+//6) e andiamo ad assegnarli alle proprieta continente dell'array
+// il risultato sara =>
+// [
+//     { name: 'italia', continent: 0 },
+//     { name: 'nigeria', continent: 1 },
+//     { name: 'australia', continent: 2 },
+//     { name: 'colombia', continent: 3 },
+//     { name: 'spagna', continent: 0 }
+//   ]
+// al continet rispondera la posizione all'interno del enum Europa è a indice 0 , africa a indice 1 e cosi via
+//7) se volessimo cambiare la numerazione dell'indice ci basta mettere = 1 nella prima posizione dell enum
+enum Continent{
+    Europa = 1,
+    Africa,
+    Oceania,
+    SudAmerica,
+    Asia
 }
-// const india = new Country<string>("india","IN")
-const india = new Country<{name:string,percentage:number}>("india","IN")
-// india.addLanguage("hindi")
-// india.addLanguage("bangali")
-//6) adesso dopo che la nostra classe è diventata generica possiamo passare ai linguaggi un oggetto 
-//7) chiaramente ci dira che non è una stringa e questo perche a country gli abbiamo dato il type string 
-//8)per risolvere ci basta dare a country il type oggetto con le proprieta name e percentage
-india.addLanguage({name:"hindi", percentage: 57})
-india.addLanguage({name:"bangali", percentage: 33})
-console.log(india)
-
-
+const Country =[
+    {
+        name: "italia",
+        continent: Continent.Europa
+    },
+    {
+        name: "nigeria",
+        continent: Continent.Africa
+    },
+    {
+        name: "australia",
+        continent: Continent.Oceania
+    },
+    {
+        name: "colombia",
+        continent: Continent.SudAmerica
+    },
+    {
+        name: "spagna",
+        continent: Continent.Europa
+    },
+]
+console.log(Country)
+//8) possiamo mettere la enum anche dentro delle interface 
+// in modo da poterla riutilizzare
+interface Country{
+    name:string,
+    continent: Continent
+}
