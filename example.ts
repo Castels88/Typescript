4a89f6e1810e86397891b29bc398d24825f5daf8
[elio] <[castels@hotmail.it]>
2022-10-02 17:14:53 +0100

    fine lezione

--- example.ts
+++ example.ts
@@ -1,29 +1,21 @@
-//Enums permette di definire un set di tag value
-// 1) mettiamo caso in questo array con oggetti abbiamo 
-// 2) degli oggetti che hanno lo stesso continente ma tutti hanno la proprieta continente
-// 3) possimao usare gli enum per rappresentare tutti questi oggetti
-//4) gli enum si creano cosi 
-//5) all 'interno del enum mettiamo tutte le categorie che vogliamo che 
-// rappresentino gli oggetti
-//6) e andiamo ad assegnarli alle proprieta continente dell'array
-// il risultato sara =>
-// [
-//     { name: 'italia', continent: 0 },
-//     { name: 'nigeria', continent: 1 },
-//     { name: 'australia', continent: 2 },
-//     { name: 'colombia', continent: 3 },
-//     { name: 'spagna', continent: 0 }
-//   ]
-// al continet rispondera la posizione all'interno del enum Europa è a indice 0 , africa a indice 1 e cosi via
-//7) se volessimo cambiare la numerazione dell'indice ci basta mettere = 1 nella prima posizione dell enum
+// posso anche esistere le enum stringhe 
+// andiamo a vedere, praticamente quando facciamo il 
+// console.log ai paesi ritornano gli indici numerici dell'enum 
+// se volessimo che tornino delle stringhe ci basta modificare 
+// le proprieta degli enum con = "un qualcosa"
+// il valore dell enum ora sara una stringa
 enum Continent{
-    Europa = 1,
-    Africa,
-    Oceania,
-    SudAmerica,
-    Asia
+    Europa = "Europa",
+    Africa = "Africa",
+    Oceania = "Oceania",
+    SudAmerica = "SudAmerica",
+    Asia = "Asia"
 }
-const Country =[
+interface Country{
+    name:string,
+    continent: Continent
+}
+const Countries: Country[] =[
     {
         name: "italia",
         continent: Continent.Europa
@@ -45,10 +37,5 @@ const Country =[
         continent: Continent.Europa
     },
 ]
-console.log(Country)
-//8) possiamo mettere la enum anche dentro delle interface 
-// in modo da poterla riutilizzare
-interface Country{
-    name:string,
-    continent: Continent
-}
+console.log(Countries)
+
